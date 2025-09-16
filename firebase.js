8f4433c400280982a5af7fea48ddfc0240c847be
BvB-Pro <abdul-rahman.itani@hotmail.de>
2025-09-16 22:06:24 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-// === Deine Firebase Config ===
 const firebaseConfig = {
   apiKey: "AIzaSyBQNtE_uMdMC1Wb8HnOjyteNIklvd7_eqU",
   authDomain: "hammouda-itani-stiftung.firebaseapp.com",
@@ -14,15 +12,14 @@ const firebaseConfig = {
   measurementId: "G-5PVQMJR6EK"
 };
 
-// Firebase starten
 export const app  = initializeApp(firebaseConfig);
 export const db   = getFirestore(app);
 export const auth = getAuth(app);
 
-// Anonyme Anmeldung
+// anonym einloggen
 signInAnonymously(auth).catch(console.error);
 
-// Promise → app.js wartet bis eingeloggt
+// darauf wartet app.js
 export const authReady = new Promise((resolve) => {
   onAuthStateChanged(auth, (user) => resolve(user));
 });
