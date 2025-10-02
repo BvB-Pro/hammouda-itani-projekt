e4d2b7e1e296806c177bde04d09861877370da0e
BvB-Pro <abdul-rahman.itani@hotmail.de>
2025-10-02 14:36:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -709,7 +709,7 @@ Alle eingegebenen Daten sind **fiktiv** und haben keinerlei Bezug zu realen Pers
 Die Plattform simuliert realistische Dokumentationsabläufe – modern, sicher und zentral synchronisiert – 
 um praxisnahes Lernen zu ermöglichen.
 .`));
-.`));
+
 
   // 2) Stiftungsleitung nebeneinander
   const grid = ce("section",{className:"board-grid"});
