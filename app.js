0130d2d984e474be2bf79264eb81129e3ee4a5bd
BvB-Pro <abdul-rahman.itani@hotmail.de>
2025-10-02 15:46:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -673,7 +673,7 @@ hero.innerHTML = `
   <div class="card hero-card">
     ${page.logo ? `<img src="${logoSrc}" alt="${page.title} Logo" class="hero-logo" onerror="this.style.display='none'">` : ""}
     <div>
-      <h1>${page.title}</h1>
+     <h1>${page.title} <span style="font-weight:normal">(Simulierte Übung)</span></h1>
       <p>${page.slogan}</p>
     </div>
   </div>`;
